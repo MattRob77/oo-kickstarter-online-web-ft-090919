cd426d2bfc321ed8b43493b784a2f6e2b25b16e3
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-19 23:02:45 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,10 @@
 class Backer 
-  attr_accessor :name, :backedprojects 
+  attr_reader :name, :backedprojects 
   
   def initialize(name)
     @name = name 
     @backed_projects = [] 
   end 
+end 
   
  
