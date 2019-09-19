34bee11baf345bbeee6c77217d99949d6e806906
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-19 23:03:26 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,17 @@
-class Backer 
-  attr_reader :name, :backedprojects 
-  
-  def initialize(name)
-    @name = name 
-    @backed_projects = [] 
-  end 
-  
+class Backer
+
+attr_reader :name, :backed_projects
+
+def initialize(name)
+    @name = name
+    @backed_projects = []
+end
+
+def back_project(project)
+    @backed_projects << project
+    project.backers << self
+end
+
+end
+
  
